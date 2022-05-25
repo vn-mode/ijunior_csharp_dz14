64c5514056b3e937424e45936de1eeb651201d74
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-25 06:14:14 +0300

    dz14
    dz14

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace vn_mode_dz_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userSymbol;
+            int lengthUserName;
+            int lengthUserSymbol;
+
+            Console.Write("Введите желаемое имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Введите желаемый символ: ");
+            userSymbol = Console.ReadLine();
+            lengthUserName = userName.Length + 2;
+            lengthUserSymbol = userSymbol.Length;
+
+            if (lengthUserSymbol > 1)
+            {
+                userSymbol = userSymbol.Substring(0, lengthUserSymbol - (lengthUserSymbol - 1));
+            }
+
+            for (int i = 0; i < lengthUserName; i++)
+            {
+                Console.Write(userSymbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(userSymbol + userName + userSymbol);
+
+            for (int i = 0; i < lengthUserName; i++)
+            {
+                Console.Write(userSymbol);
+            }
+
+        }
+    }
+}
